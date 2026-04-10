0174a992f3e714bf462b4c742df506b9eeed1fe2
Ben Elliott <ellio747@umn.edu>
2026-04-10 06:05:27 -0500

    complete Part II final commit after updates

--- R/week11-local.R
+++ R/week11-local.R
@@ -6,58 +6,56 @@ library(caret)
 library(xgboost)
 library(parallel) # added `parallel` package to conduct parallelized models
 library(doParallel) # added `doParallel` package to conduct parallelized models
-# library(tictoc)
-# library(microbenchmark) # this will provide an operation to provide a whats faster
 set.seed(123) 
 
 # Data Import and Cleaning
-gss_tbl <- haven::read_sav("../data/GSS2016.sav", user_na = T) %>% 
+gss_import_tbl <- haven::read_sav("../data/GSS2016.sav", user_na = T) %>% # I updated the import tibble as discussed in class outbrief
   haven::zap_missing() %>% 
-  filter(!is.na(mosthrs)) %>% 
-  rename(`work hours` = mosthrs) %>% 
+  filter(!is.na(mosthrs))
+gss_tbl <- gss_import_tbl %>% 
   select(-c(hrs1, hrs2)) %>% 
   select(which(colMeans(is.na(.)) < 0.75)) %>% 
-  mutate(across(where(haven::is.labelled), as.numeric)) %>% 
-  mutate(`work hours` = as.numeric(`work hours`)) %>% 
+  mutate(across(everything(), as.numeric)) %>% 
   as_tibble() 
 
 # Visualization
-(ggplot(gss_tbl, aes(x = `work hours`)) + 
+(ggplot(gss_tbl, aes(x = mosthrs)) + 
   geom_histogram(binwidth = 4)) %>%  
   ggsave(filename = "../figs/hours_histogram.png", width = 1920, height = 1080, units = "px", dpi = 300)
 
 # Analysis 
 
 ## Define training and test sets
-holdout_indices <- createDataPartition(gss_tbl$`work hours`, p = .25, list=F) 
-gss_training <- gss_tbl[holdout_indices,] 
-gss_holdout <- gss_tbl[-holdout_indices,] 
+holdout_indices <- createDataPartition(gss_tbl$mosthrs, p = .25, list=F) # I had this structure incorrect in Week 10 project; updated after outbrief
+gss_holdout <- gss_tbl[holdout_indices,] 
+gss_training <- gss_tbl[-holdout_indices,] 
 
 ## Run k-fold testing hyperparameters on training set
 # I modified each of the models below by adding `system.time` and storing the results in a new variable, which I then added to my table2_tbl
 
-mod1_tm <- system.time({ # added wrapper for system.time around all models, storing the variable for table2_tbl construction
+mod1_tm <- system.time({ # added wrapper for system.time around all models, storing the variable for table2_tbl construction; experimented with library(tictoc) and library(microbenchmark)
   model1 <- train(
-    `work hours` ~ ., 
+    mosthrs ~ ., 
     gss_training, 
     na.action = na.pass, 
     method = "lm", 
-    preProcess = c("zv", "medianImpute","center","scale"), 
+    preProcess = c("medianImpute","center","nzv", "scale"), 
     trControl=trainControl(
       method="cv", number=10, verboseIter=T 
     )
   )
 })
 
+
 local_cluster <- makeCluster(7) # using `detectCores()` I identified 8 cores, subtracting 1, I began the local cluster for parallelization
 registerDoParallel(local_cluster) # activate cluster
 mod1_tm_par <- system.time({
   model1 <- train(
-    `work hours` ~ ., 
+    mosthrs ~ ., 
     gss_training, 
     na.action = na.pass, 
     method = "lm", 
-    preProcess = c("zv", "medianImpute","center","scale"), 
+    preProcess = c("medianImpute","center","nzv", "scale"),  
     trControl=trainControl(
       method="cv", number=10, verboseIter=T 
     )
@@ -66,13 +64,14 @@ mod1_tm_par <- system.time({
 stopCluster(local_cluster) # deactivate cluster
 registerDoSEQ() # explicit registration of the backend of a cluster
 
+
 mod2_tm <- system.time({
   model2 <- train(
-    `work hours` ~ ., 
+    mosthrs ~ ., 
     gss_training, 
     na.action = na.pass, 
     method = "glmnet", 
-    preProcess = c("zv", "medianImpute","center","scale"),
+    preProcess = c("medianImpute","center","nzv", "scale"), 
     tuneGrid = expand.grid( 
       alpha = c(0,1), 
       lambda = seq(0.0001, 0.1, length = 10) 
@@ -83,14 +82,15 @@ mod2_tm <- system.time({
   )
 })
 
+local_cluster <- makeCluster(7) # activate cluster
 registerDoParallel(local_cluster) # start cluster again
 mod2_tm_par <- system.time({
   model2 <- train(
-    `work hours` ~ ., 
+    mosthrs ~ ., 
     gss_training, 
     na.action = na.pass, 
     method = "glmnet", 
-    preProcess = c("zv", "medianImpute","center","scale"),
+    preProcess = c("medianImpute","center","nzv", "scale"), 
     tuneGrid = expand.grid( 
       alpha = c(0,1), 
       lambda = seq(0.0001, 0.1, length = 10) 
@@ -105,27 +105,28 @@ registerDoSEQ() # explicit registration of cluster backend
 
 mod3_tm <- system.time({
   model3 <- train(
-    `work hours` ~ ., 
+    mosthrs ~ ., 
     gss_training, 
     na.action = na.pass, 
     method = "ranger", 
-    preProcess = c("zv", "medianImpute","center","scale"),
-    tuneLength = 5,
+    preProcess = c("medianImpute","center","nzv", "scale"), 
+    tuneLength = 3, # I revised my week 10 code to simplify with tuneLength rather than tuneGrid for code optimization
     trControl=trainControl(
       method="cv", number=10, verboseIter=T 
     )
   )
 })
 
+local_cluster <- makeCluster(7) # activate cluster
 registerDoParallel(local_cluster) # restart cluster
 mod3_tm_par <- system.time({
   model3 <- train(
-    `work hours` ~ ., 
+    mosthrs ~ ., 
     gss_training, 
     na.action = na.pass, 
     method = "ranger", 
-    preProcess = c("zv", "medianImpute","center","scale"),
-    tuneLength = 5,
+    preProcess = c("medianImpute","center","nzv", "scale"), 
+    tuneLength = 3, # I revised my week 10 code to simplify with tuneLength rather than tuneGrid for code optimization
     trControl=trainControl(
       method="cv", number=10, verboseIter=T 
     )
@@ -136,27 +137,28 @@ registerDoSEQ() # explicit registration of cluster backend
 
 mod4_tm <- system.time({ 
   model4 <- train(
-    `work hours` ~., 
+    mosthrs ~., 
     gss_training, 
     na.action = na.pass, 
     method = "xgbTree", 
-    preProcess = c("zv", "medianImpute", "center", "scale"),
-    tuneLength = 5,
+    preProcess = c("medianImpute","center","nzv", "scale"), 
+    tuneLength = 3, # I revised my week 10 code to simplify with tuneLength rather than tuneGrid for code optimization
     trControl=trainControl(
       method="cv", number=10, verboseIter=T 
     ) 
   )
 })
 
+local_cluster <- makeCluster(7) # activate cluster
 registerDoParallel(local_cluster) # restart cluster
 mod4_tm_par <- system.time({ 
   model4 <- train(
-    `work hours` ~., 
+    mosthrs ~., 
     gss_training, 
     na.action = na.pass, 
     method = "xgbTree", 
-    preProcess = c("zv", "medianImpute", "center", "scale"),
-    tuneLength = 5,
+    preProcess = c("medianImpute","center","nzv", "scale"), 
+    tuneLength = 3, # I revised my week 10 code to simplify with tuneLength rather than tuneGrid for code optimization
     trControl=trainControl(
       method="cv", number=10, verboseIter=T 
     ) 
@@ -182,10 +184,10 @@ table1_tbl <- tibble(
                               getTrainPerf(model2)$TrainRsquared, 
                               getTrainPerf(model3)$TrainRsquared,
                               getTrainPerf(model4)$TrainRsquared), 2), "^0"),
-  ho_rsq = str_remove(round(c(postResample(pred1, gss_holdout$`work hours`)["Rsquared"], 
-                              postResample(pred2, gss_holdout$`work hours`)["Rsquared"], 
-                              postResample(pred3, gss_holdout$`work hours`)["Rsquared"], 
-                              postResample(pred4, gss_holdout$`work hours`)["Rsquared"]), 2), "^0")
+  ho_rsq = str_remove(round(c(postResample(pred1, gss_holdout$mosthrs)["Rsquared"], 
+                              postResample(pred2, gss_holdout$mosthrs)["Rsquared"], 
+                              postResample(pred3, gss_holdout$mosthrs)["Rsquared"], 
+                              postResample(pred4, gss_holdout$mosthrs)["Rsquared"]), 2), "^0")
 ) %>% 
   write_csv(file = "../figs/table1.csv") 
 
@@ -197,5 +199,5 @@ table2_tbl <- tibble(
   write_csv(file = "../figs/table2.csv") 
 
 # The non-linear tree-based models benefited most from parallelization. This is because parallelization increased the logical processors that could conduct the simultaneous operations required to reduce the speed of more complex models. 
-# The largest difference for the fastest model was a 198 second increase to speed in the xgbTree model. The elastic net model which was parallelized was actually .82 seconds slower. This is because xgbTree is very complex, where the unparallelized model has a 2.55 realitive time compared to the parallelized model. 
-# I would recommend the use of model3, the Random Forest model. This is because the model has the most variance explained of all the models at the current hyperparameter tuning settings. When parallelized, the random forest model also has a relative time 4.5 times faster than the xgbTree parallelized model. Taken together the variance explained is much higher at a faster speed.  
+# The largest difference for the fastest model was a 155 second increase to speed in the xgbTree model. The OLS linear model which was parallelized was only 1.56 seconds faster. This is because xgbTree is very complex, where the unparallelized model has a 3.98 relative time compared to the parallelized model. 
+# I would recommend the use of model4, the xgbTree model. While the xgbTree model has the second most variance explained (after Random Forest - RF) of all the models at the current hyperparameter tuning settings, its out of sample difference is a bit larger than RF. When parallelized, however the xgbTree model has a relative time 1.28 times faster than the RF parallelized model. Taken together the variance explained is higher at a much faster speed.  
